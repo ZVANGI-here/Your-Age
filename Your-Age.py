6824b4e7467c5c2a1acc616ad56187af21140d08
ZVANGI-here <59018467+ZVANGI-here@users.noreply.github.com>
2020-04-28 13:16:47 -0400

    Add files via upload

---
+++ Your-Age.py
@@ -0,0 +1,103 @@
+#import date library
+
+import datetime
+from V7xStyle import Animation
+import os
+
+os.system ('clear')
+#veritable
+
+An = Animation 
+
+#codes of year and age
+
+#help(An)
+#
+print('\033[1;33;40m')
+txtlogo = r'''
+__   __                    _              
+\ \ / /__  _   _ _ __     / \   __ _  ___ 
+ \ V / _ \| | | | '__|   / _ \ / _` |/ _ \
+  | | (_) | |_| | |     / ___ \ (_| |  __/
+  |_|\___/ \__,_|_|    /_/   \_\__, |\___|
+                               |___/
+                                         @Zvangi
+
+'''
+
+An.SlowIndex(txtlogo,
+             t=0.01)
+for spac in range (4):
+             print('\033[1;97m')
+An.SlowText('Hi This is a BETA version ')
+for space in range(2):
+             print('')
+An.SlowText ('This is my first script . . . :D')
+for space in range(2):
+             print('\033[1;91m')
+An.SlowText ('Before Start i want say . .')
+print('')
+print('\033[1;94m')
+An.SlowText ('Thanks For all Friends They Heplped me')
+print('')
+print('\033[1;95m')
+An.SlowText ('Hmida donsong / houari abdellah / Yoru Ayamachi / The Fairylord ')
+for space in range(3):
+             print ('\033[1;97m')
+An.SlowText ('And Now . . !')
+print ('')
+An.SlowText ('Lets get started . . !')
+print('')
+for space in range(3):
+             print('')
+#An.slowText . . . !
+An.SlowText('\033[1;96mPlease Enter Your Name . . . !')
+print ('')
+print ('')
+name = (input ('\033[1;97===============> : '))
+print ('')
+print ('')
+An.SlowText('\033[1;96mPlease Enter The Birth Year . . . !')
+print ('')
+print ('')
+birth = int(input("\033[1;97m===============> : "))
+
+             
+age = datetime.datetime.now().year - birth
+
+print ("             "), 
+print ("             "),  
+print ("             "), 
+print (" \033[1;95m.            "),
+
+#loading page
+An.SlowText('Wait a 5 second Please . . . . ! ')
+An.SlowText('    @ZVANGI . . !')
+An.DL(),
+
+for space in range(4):
+             print("             ")
+
+An.SlowText ("         \033[1;31;40m  Your Age    ")
+print (' ')
+
+for i in range(3):
+             print ("\033[1;97m.    |  |            |    . ")
+print ("\033[1;96m.    v  V            V    . ")
+print ("\033[1;33;40m.                                          [-Hi World !-]")
+print ('')
+print ('')
+
+#print code of age
+
+print ("\033[1;97m Hi ", "\033[1;30;40m", name, "\033[1;97mYour Age is :", age, "\033[1;97mYears old")
+print ('')
+print ('')
+print ('')
+An.SlowText ('\033[1;31;40mThank For Using This Script . . . :D')
+print ('   ')
+print ('   ')
+print ('\033[1;97m')
+An.SlowText('                       Done . . . :D')
+print ('')
+print('')
